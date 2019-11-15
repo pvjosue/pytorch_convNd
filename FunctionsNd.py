8af9a4a24298a6491c02afe9d351a9e00565af0c
Josue Page <josue.page@tum.de>
2019-11-15 10:56:03 +0100

    Fixed for multiple input/output channel, and automatic conv selection deppending on input dimmension. Also bias is applied only in top-most layer

--- FunctionsNd.py
+++ FunctionsNd.py
@@ -29,6 +29,11 @@ class ConvNd(nn.Module):
         if not isinstance(padding, Tuple):
             padding = tuple(padding for _ in range(num_dims))
 
+        # This parameter defines which Pytorch convolution to use as a base, for 3 Conv2D is used
+        # for conv4D max_dims
+        max_dims = 2
+        self.conv_f = (nn.Conv1d, nn.Conv2d, nn.Conv3d)[max_dims - 1]
+        
         assert len(kernel_size) == num_dims, \
             '4D kernel size expected!'
         assert len(stride) == num_dims, \
@@ -39,8 +44,171 @@ class ConvNd(nn.Module):
             'Dilation rate other than 1 not yet implemented!'
         assert groups == 1, \
             'Groups other than 1 not yet implemented!'
-        assert num_dims >=3, \
-            'This function works for more than 3 dimensions, for less use torch implementation'
+        # assert num_dims >=max_dims, \
+        #     'This function works for more than 3 dimensions, for less use torch implementation'
+
+        # ---------------------------------------------------------------------
+        # Store constructor arguments
+        # ---------------------------------------------------------------------
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.num_dims = num_dims
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.groups = groups
+        self.use_bias = use_bias
+        self.weight = nn.Parameter(torch.Tensor(
+                out_channels, in_channels // groups, self.kernel_size[0]))
+        if use_bias:
+            self.bias = nn.Parameter(torch.Tensor(out_channels))
+        else:
+            self.register_parameter('bias', None)
+        self.bias_initializer = bias_initializer
+        self.kernel_initializer = kernel_initializer
+
+        # ---------------------------------------------------------------------
+        # Construct 3D convolutional layers
+        # ---------------------------------------------------------------------
+        if self.kernel_initializer is not None:
+            self.kernel_initializer(self.weight)
+        if self.bias_initializer is not None:
+            if self.use_bias:
+                self.bias_initializer(self.bias)
+        # Use a ModuleList to store layers to make the Conv4d layer trainable
+        self.conv_layers = torch.nn.ModuleList()
+
+        # Compute the next dimension, so for a conv4D, get index 3
+        next_dim_len = self.kernel_size[0] 
+        
+        for _ in range(next_dim_len):
+            if self.num_dims-1 != max_dims:
+                # Initialize a Conv_n-1_D layer
+                conv_layer = ConvNd(in_channels=self.in_channels,
+                                            out_channels=self.out_channels,
+                                            use_bias=self.use_bias,
+                                            num_dims=self.num_dims-1,
+                                            kernel_size=self.kernel_size[1:],
+                                            stride=self.stride[1:],
+                                            padding=self.padding[1:])
+
+            else:
+                # Initialize a Conv layer
+                # bias should only be applied by the top most layer, so we disable bias in the internal convs
+                conv_layer = self.conv_f(in_channels=self.in_channels,
+                                            out_channels=self.out_channels,
+                                            bias=False,
+                                            kernel_size=self.kernel_size[1:],
+                                            stride=self.stride[1:],
+                                            padding=self.padding[1:])
+
+            # Apply initializer functions to weight and bias tensor
+            if self.kernel_initializer is not None:
+                self.kernel_initializer(conv_layer.weight)
+
+            # Store the layer
+            self.conv_layers.append(conv_layer)
+
+    # -------------------------------------------------------------------------
+
+    def forward(self, input):
+        # Define shortcut names for dimensions of input and kernel
+        (b, c_i) = tuple(input.shape[0:2])
+        size_i = tuple(input.shape[2:])
+        size_k = self.kernel_size
+
+        # Compute the size of the output tensor based on the zero padding
+        size_o = tuple([math.floor((size_i[x] + 2 * self.padding[x] - size_k[x]) / self.stride[x] + 1) for x in range(len(size_i))])
+        # (math.floor((l_i + 2 * self.padding - l_k) / self.stride + 1),
+
+        # Compute size of the output without stride
+        size_ons = tuple([size_i[x] + 2 * self.padding[x] - size_k[x] + 1 for x in range(len(size_i))])
+
+
+        # Output tensors for each 3D frame
+        frame_results = size_o[0] * [torch.zeros((b,c_i) + size_o[1:]).to(input.device)] # todo: add .cuda()
+        empty_frames = size_o[0] * [None]
+
+        # Convolve each kernel frame i with each input frame j
+        for i in range(size_k[0]):
+
+            for j in range(size_i[0]):
+
+                # Add results to this output frame
+                out_frame = j - (i - size_k[0] // 2) - (size_i[0] - size_ons[0]) // 2 - (1-size_k[0]%2) 
+                k_center_position = out_frame % self.stride[0]
+
+                out_frame = math.floor(out_frame / self.stride[0])
+                if out_frame < 0 or out_frame >= size_o[0] or k_center_position != 0:
+                    continue
+
+                # Prepate input for next dimmension
+                conv_input = input.view(b, c_i, size_i[0], -1)
+                conv_input = conv_input[:, :, j, :].view((b, c_i) + size_i[1:])
+
+                # Convolve
+                frame_conv = \
+                    self.conv_layers[i](conv_input)
+
+                # Store in computed output position at current dimension
+                if empty_frames[out_frame] is None:
+                    frame_results[out_frame] = frame_conv
+                    empty_frames[out_frame] = 1
+                else:
+                    frame_results[out_frame] += frame_conv
+
+        result = torch.stack(frame_results, dim=2)
+        if self.use_bias:
+            resultShape = result.shape
+            result = result.view(b,resultShape[1],-1)
+            for k in range(self.out_channels):
+                result[:,k,:] += self.bias[k]
+            return result.view(resultShape)
+        else:
+            return result
+
+
+class ConvTransposeNd(nn.Module):
+    """Some Information about ConvNd"""
+    def __init__(self,in_channels: int,
+                 out_channels: int,
+                 num_dims: int,
+                 kernel_size: Tuple,
+                 stride,
+                 padding,
+                 dilation: int = 1,
+                 groups: int = 1,
+                 use_bias: bool = True,
+                 bias_initializer: Callable = None,
+                 kernel_initializer: Callable = None):
+        super(ConvTransposeNd, self).__init__()
+
+        # ---------------------------------------------------------------------
+        # Assertions for constructor arguments
+        # ---------------------------------------------------------------------
+        if not isinstance(kernel_size, Tuple):
+            kernel_size = tuple(kernel_size for _ in range(num_dims))
+        if not isinstance(stride, Tuple):
+            stride = tuple(stride for _ in range(num_dims))
+        if not isinstance(padding, Tuple):
+            padding = tuple(padding for _ in range(num_dims))
+
+        # This parameter defines which Pytorch convolution to use as a base, for 3 Conv2D is used
+        max_dims = 2
+        
+        assert len(kernel_size) == num_dims, \
+            '4D kernel size expected!'
+        assert len(stride) == num_dims, \
+            '4D stride size expected!'
+        assert len(padding) == num_dims, \
+            '4D padding size expected!'
+        assert dilation == 1, \
+            'Dilation rate other than 1 not yet implemented!'
+        assert groups == 1, \
+            'Groups other than 1 not yet implemented!'
+        # assert num_dims >=max_dims, \
+        #     'This function works for more than 3 dimensions, for less use torch implementation'
 
 # ---------------------------------------------------------------------
         # Store constructor arguments
@@ -54,19 +222,18 @@ class ConvNd(nn.Module):
         self.padding = padding
         self.groups = groups
         self.use_bias = use_bias
-        self.weight = torch.nn.Parameter(data=torch.ones(1,num_dims), requires_grad=True)
-        self.bias = torch.nn.Parameter(data=torch.ones(1,num_dims), requires_grad=True)
+        self.bias = torch.nn.Parameter(data=torch.ones(out_channels), requires_grad=True)
         self.bias_initializer = bias_initializer
         self.kernel_initializer = kernel_initializer
 
         # ---------------------------------------------------------------------
         # Construct 3D convolutional layers
         # ---------------------------------------------------------------------
-        if self.kernel_initializer is not None:
-            self.kernel_initializer(self.weight)
-            if self.bias_initializer is not None:
-                if self.use_bias:
-                    self.bias_initializer(self.bias)
+        # if self.kernel_initializer is not None:
+            # self.kernel_initializer(self.weight)
+        if self.bias_initializer is not None:
+            if self.use_bias:
+                self.bias_initializer(self.bias)
         # Use a ModuleList to store layers to make the Conv4d layer trainable
         self.conv_layers = torch.nn.ModuleList()
 
@@ -74,9 +241,9 @@ class ConvNd(nn.Module):
         next_dim_len = self.kernel_size[0] 
         
         for _ in range(next_dim_len):
-            if self.num_dims-1 != 3:
+            if self.num_dims-1 != max_dims:
                 # Initialize a Conv_n-1_D layer
-                conv_layer = ConvNd(in_channels=self.in_channels,
+                conv_layer = ConvTransposeNd(in_channels=self.in_channels,
                                             out_channels=self.out_channels,
                                             use_bias=self.use_bias,
                                             num_dims=self.num_dims-1,
@@ -86,7 +253,7 @@ class ConvNd(nn.Module):
 
             else:
                 # Initialize a Conv3D layer
-                conv_layer = torch.nn.Conv3d(in_channels=self.in_channels,
+                conv_layer = torch.nn.ConvTranspose2d(in_channels=self.in_channels,
                                             out_channels=self.out_channels,
                                             bias=self.use_bias,
                                             kernel_size=self.kernel_size[1:],
@@ -112,12 +279,12 @@ class ConvNd(nn.Module):
         size_k = self.kernel_size
 
         # Compute the size of the output tensor based on the zero padding
-        size_o = tuple([math.floor((size_i[x] + 2 * self.padding[x] - size_k[x]) / self.stride[x] + 1) for x in range(len(size_i))])
+        size_o = tuple([(size_i[x] - 1) * self.stride[x] - 2 * self.padding[x] + (size_k[x]-1) + 1 for x in range(len(size_i))])
         # (math.floor((l_i + 2 * self.padding - l_k) / self.stride + 1),
 
         # Compute size of the output without stride
-        size_onp = tuple([size_i[x] + 2 * self.padding[x] - size_k[x] + 1 for x in range(len(size_i))])
-        # l_i + 2 * self.padding - l_k + 1
+        size_ons = tuple([(size_i[x] - 1) - 2 * self.padding[x] + size_k[x] + 1 for x in range(len(size_i))])
+
 
         # Output tensors for each 3D frame
         frame_results = size_o[0] * [torch.zeros((b,c_i) + size_o[1:]).cuda()] # todo: add .cuda()
@@ -129,17 +296,16 @@ class ConvNd(nn.Module):
             for j in range(size_i[0]):
 
                 # Add results to this output frame
-                out_frame = j - (i - size_k[0] // 2) - (size_i[0] - size_onp[0]) // 2 - (1-size_k[0]%2) 
-                k_center_position = out_frame % self.stride[0]
+                out_frame = j * self.stride[0] - (i - size_k[0] // 2) + (size_i[0] - size_ons[0]) // 2 +1 #- 2*self.padding[0] - (1-size_k[0]%2)
 
-                out_frame = math.floor(out_frame / self.stride[0])
-                if out_frame < 0 or out_frame >= size_o[0] or k_center_position != 0:
+                if out_frame < 0 or out_frame >= size_o[0]:
                     continue
 
+                # Prepate input for next dimmension
                 conv_input = input.view(b, c_i, size_i[0], -1)
                 conv_input = conv_input[:, :, j, :].view((b, c_i) + size_i[1:])
-                # if self.num_dims==4:
-                    # conv_input = F.pad(conv_input,(self.padding[0],self.padding[0],self.padding[1],self.padding[1],self.padding[2],self.padding[2]), mode='reflect')
+
+                # Convolve
                 frame_conv = \
                     self.conv_layers[i](conv_input)
 
@@ -150,3 +316,28 @@ class ConvNd(nn.Module):
                     frame_results[out_frame] += frame_conv
 
         return torch.stack(frame_results, dim=2)
+
+
+
+
+class DenseBlock(nn.Module):
+    def __init__(self, nChans, ks):
+        super(DenseBlock, self).__init__()
+        padding = math.floor(ks/2)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(nChans, nChans, ks, padding=padding),
+            nn.ReLU())
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(nChans, nChans, ks, padding=padding),
+            nn.ReLU())
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(nChans, nChans, ks, padding=padding),
+            nn.ReLU())
+        self.convOut = nn.Conv2d(nChans,nChans, kernel_size=1)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.conv2(x+x1)
+        x3 = self.conv3(x+x1+x2)
+        x4 = self.convOut(x+x1+x2+x3)
+        return x+x4
--- main.py
+++ main.py
@@ -2,24 +2,33 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from FunctionsNd import ConvNd
+from FunctionsNd import ConvTransposeNd
 import time
 import matplotlib.pyplot as plt
-from conv4d import Conv4d
+torch.backends.cudnn.deterministic = True
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
-x = torch.randn((4, 1, 50, 50, 10)).to(device)
+inChans = 3
+outChans = 91
+x = torch.rand((2, inChans, 51, 51, 51)).to(device)
 
-ks = (5, 5, 5)
-padding = 2
-stride = 1
+ks = (3, 3, 3)
+padding = 1
+stride = 4
+weight = 2
+bias = 0
+conv = ConvNd(inChans, outChans, 3, ks, stride, padding, use_bias=True, 
+kernel_initializer=lambda x: torch.nn.init.constant_(x, weight), 
+bias_initializer=lambda x: torch.nn.init.constant_(x, bias)).to(device)
 
-conv = ConvNd(1, 1, 3, ks, stride, padding, 
-kernel_initializer=lambda x: torch.nn.init.constant_(x, 1), 
-bias_initializer=lambda x: torch.nn.init.constant_(x, 0)).to(device)
+convT = ConvTransposeNd(1, 1, 3, ks, stride, padding, 
+kernel_initializer=lambda x: torch.nn.init.constant_(x, weight), 
+bias_initializer=lambda x: torch.nn.init.constant_(x, bias)).to(device)
 
 start = time.time()
 out = conv(x)
+# outT = convT(x)
 print("mine")
 end = time.time()
 print(end - start)
@@ -27,27 +36,41 @@ print(end - start)
 useGT = True
 
 if useGT:
-    convGT = nn.Conv3d(1, 1, ks, stride, padding=padding)
-    torch.nn.init.constant_(convGT.weight, 1)
-    torch.nn.init.constant_(convGT.bias, 0).to(device)
+    convGT = nn.Conv3d(inChans, outChans, ks, stride, padding=padding, bias=True)
+    torch.nn.init.constant_(convGT.weight, weight)
+    torch.nn.init.constant_(convGT.bias, bias).to(device)
+
+    # convGTT = nn.ConvTranspose3d(1, 1, ks, stride, padding=padding)
+    # torch.nn.init.constant_(convGTT.weight, weight)
+    # torch.nn.init.constant_(convGTT.bias, bias).to(device)
 
     convGT = convGT.to(device)
+    # convGTT = convGTT.to(device)
 
 
     start = time.time()
     outGT = convGT(x)
+    # outGTT = convGTT(x)
     print("GT")
     end = time.time()
     print(end - start)
     diff = abs(out-outGT)
+    # diffT = abs(outT-outGTT)
     print(torch.sum(diff))
+    # print(torch.sum(diffT))
 
 # print(out.shape)
-# plt.figure(1)
-# plt.subplot(221)
-# plt.imshow(x[0,0,:,:,:,:].sum(2).sum(2).cpu().data.detach())
-# plt.subplot(222)
-# plt.imshow(out[0,0,:,:,:,:].sum(2).sum(2).cpu().data.detach())
-# plt.subplot(223)
-# plt.imshow(outGT[0,0,:,:,:,:].sum(2).sum(2).cpu().data.detach())
-# plt.show()
+plt.figure(1)
+plt.subplot(221)
+plt.imshow(x[0,0,:,:,:].sum(2).cpu().data.detach())
+plt.title('input')
+plt.subplot(222)
+plt.imshow(out[0,:,:,:,:].sum(2).sum(1).cpu().data.detach())
+plt.title('convND out')
+plt.subplot(223)
+plt.imshow(outGT[0,:,:,:,:].sum(2).sum(1).cpu().data.detach())
+plt.title('GT out')
+plt.subplot(224)
+plt.imshow(diff[0,:,:,:,:].sum(2).sum(1).cpu().data.detach())
+plt.title('diff out')
+plt.show()
